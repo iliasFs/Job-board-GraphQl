b5b8285928861af9c771a79f005bd2cb6ccf42a0
iliasfasilakis <ilias.fasilakhs@gmail.com>
2023-10-09 12:09:50 +0300

    setting express with apollo server and Graphql endpoint-sandbox

--- server/server.js
+++ server/server.js
@@ -1,14 +1,26 @@
-import cors from 'cors';
-import express from 'express';
-import { authMiddleware, handleLogin } from './auth.js';
+import cors from "cors";
+import express from "express";
+import { authMiddleware, handleLogin } from "./auth.js";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware as apolloMiddleware } from "@apollo/server/express4";
+//we need to read the graphql schema file
+import { readFile } from "node:fs/promises";
+import { resolvers } from "./resolvers.js";
 
 const PORT = 9000;
 
 const app = express();
 app.use(cors(), express.json(), authMiddleware);
 
-app.post('/login', handleLogin);
+app.post("/login", handleLogin);
+
+const typeDefs = await readFile("./schema.graphql", "utf8");
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+await apolloServer.start();
+app.use("/graphql", apolloMiddleware(apolloServer));
 
 app.listen({ port: PORT }, () => {
   console.log(`Server running on port ${PORT}`);
+  console.log(`GraphQl endpoint: http://localhost:${PORT}/graphql`);
 });
