68e5b1b6fc7e8ae49d9123d075d052cecaf1a7da
iliasfasilakis <ilias.fasilakhs@gmail.com>
2023-10-09 14:47:08 +0300

    object types, non nullability, field resolvers, database access

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,5 +1,25 @@
+import { getJobs } from "./db/jobs.js";
+
 export const resolvers = {
+  //   Query: {
+  //     jobs: async () => {
+  //       const jobs = await getJobs();
+  //       console.log(jobs);
+  //       return jobs;
+  //     },
+  //   },
+
   Query: {
-    greeting: () => "Hello world",
+    jobs: () => getJobs(),
+  },
+  Job: {
+    date: (job) => {
+      console.log("resolving the date for job:", job);
+      return toIsoDate(job.createdAt);
+    },
   },
 };
+
+function toIsoDate(value) {
+  return value.slice(0, "yyyy-mm-dd".length);
+}
