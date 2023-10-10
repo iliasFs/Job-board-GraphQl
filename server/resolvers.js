4ba27dc973af69f45cfe5bb10965d72d1943fb5e
iliasfasilakis <ilias.fasilakhs@gmail.com>
2023-10-10 12:18:14 +0300

    error handling-custom errors

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,11 +1,24 @@
 import { getCompany } from "./db/companies.js";
 import { getJob, getJobs, getJobsByCompany } from "./db/jobs.js";
+import { GraphQLError } from "graphql";
 
 export const resolvers = {
   Query: {
-    job: (_root, { id }) => getJob(id),
+    job: async (_root, { id }) => {
+      const job = await getJob(id);
+      if (!job) {
+        throw notFoundError("no job found with this id: " + id);
+      }
+      return job;
+    },
     jobs: () => getJobs(),
-    company: (_root, { id }) => getCompany(id),
+    company: async (_root, { id }) => {
+      const company = await getCompany(id);
+      if (!company) {
+        throw notFoundError("No company found with id: " + id);
+      }
+      return company;
+    },
   },
 
   Company: {
@@ -22,6 +35,12 @@ export const resolvers = {
     },
   },
 };
+
+function notFoundError(message) {
+  return new GraphQLError(message, {
+    extensions: { code: "NOT-FOUND" },
+  });
+}
 
 function toIsoDate(value) {
   return value.slice(0, "yyyy-mm-dd".length);
